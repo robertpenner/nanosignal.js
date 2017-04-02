0bbb20ef98278168ccbd5c9c6137d579981a4adf
Robert Penner <robert@robertpenner.com>
2017-04-02 12:59:21 -0700

    focus on ES5 implementation

--- src/nanosignal.js
+++ src/nanosignal.js
@@ -1,29 +1,26 @@
-/// <reference path="nanosignal_interfaces.d.ts" />
-function signal(options) {
-    var listeners = [];
-    var nanosignal = function () {
-        for (var i = 0, fns = listeners, len = fns.length; i < len; i++)
-            fns[i].apply(null, arguments);
-        if (options === 'once')
-            listeners = [];
-    };
-    /**
-     * Starts a function listening to this signal.
-     * @param listener
-     */
-    nanosignal.add = function (listener) {
-        if (listeners.indexOf(listener) < 0)
-            listeners = listeners.concat(listener);
-    };
-    /**
-     * Stops a function from listening to this signal.
-     * Removes all listeners if one is not specified.
-     * @param listener
-     */
-    nanosignal.remove = function (listener) {
-        listeners = !arguments.length ? [] : listeners.filter(function (fn) { return fn !== listener; });
-    };
-    return nanosignal;
+"use strict";
+function signal() {
+  var listeners = [];
+  var nanosignal = function () {
+    var args = arguments;
+    return listeners.map(function (fn) { return fn.apply(null, args); });
+  };
+
+  /**
+   * Starts a function listening to this signal.
+   * @param listener
+   */
+  nanosignal.add = function (listener) {
+    listeners = listeners.concat(listener);
+  };
+
+  /**
+   * Stops a function from listening to this signal.
+   * @param listener
+   */
+  nanosignal.remove = function (listener) {
+    listeners = listeners.filter(function (fn) { return fn !== listener; });
+  };
+  return nanosignal;
 }
 module.exports = signal;
-//# sourceMappingURL=nanosignal.js.map
