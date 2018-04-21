2efcca77f01c4d7209577d15f13519ce288d08ad
Robert Penner <robert@robertpenner.com>
2018-04-21 18:39:29 -0400

    copy over nanosignal files

--- src/nanosignal.ts
+++ src/nanosignal.ts
@@ -1,4 +1,42 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+export interface Listener<PAYLOAD, RESULT = void> {
+  (payload: PAYLOAD): RESULT
+}
+
+/**
+ * Cancels a listener's subscription to its signal.
+ */
+export interface Unsubscriber {
+  (): void
+}
+
+export interface Signal<PAYLOAD, RESULT = void> {
+  (payload: PAYLOAD): ReadonlyArray<RESULT>
+
+  /**
+   * Starts a function listening to this signal.
+   * @param listener
+   */
+  subscribe(listener: Listener<PAYLOAD, RESULT>): Unsubscriber
+}
+
+/**
+ * Creates a Signal: a function that multicasts a payload to listeners.
+ * @returns A new Signal
+ */
+export default function createSignal<PAYLOAD, RESULT = void>(
+  firstListener?: Listener<PAYLOAD, RESULT>
+): Signal<PAYLOAD, RESULT> {
+  let listeners: ReadonlyArray<Listener<PAYLOAD, RESULT>> = firstListener ? [firstListener] : []
+
+  const signal: Partial<Signal<PAYLOAD, RESULT>> = (payload: PAYLOAD) =>
+    listeners.map(fn => fn(payload))
+
+  signal.subscribe = listener => {
+    listeners = listeners.concat(listener)
+    return () => {
+      listeners = listeners.filter(fn => fn !== listener)
+    }
+  }
+
+  return signal as Signal<PAYLOAD, RESULT>
+}
--- test/nanosignal.test.ts
+++ test/nanosignal.test.ts
@@ -1,14 +1,60 @@
-import DummyClass from '../src/nanosignal'
+import createSignal, { Signal, Unsubscriber } from '../src/nanosignal'
 
-/**
- * Dummy test
- */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+describe('Signal', () => {
+  let dated: Signal<Date>
+  beforeEach(() => {
+    dated = createSignal()
+  })
+  it('is a function', () => expect(typeof dated).toBe('function'))
+
+  describe('given 2 listeners', () => {
+    let listenerA: jest.Mock<any>
+    let listenerB: jest.Mock<any>
+    const payload = new Date()
+    beforeEach(() => {
+      listenerA = jest.fn()
+      listenerB = jest.fn()
+      dated.subscribe(listenerA)
+      dated.subscribe(listenerB)
+      dated(payload)
+    })
+
+    it('when the signal fires, the listeners receive the payload', () => {
+      expect(listenerA).toHaveBeenCalledWith(payload)
+      expect(listenerB).toHaveBeenCalledWith(payload)
+    })
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  describe('given a firstListener argument and then subscribe() a second listener', () => {
+    let listenerA: jest.Mock<any>
+    let listenerB: jest.Mock<any>
+    const payload = new Date()
+    beforeEach(() => {
+      listenerA = jest.fn()
+      listenerB = jest.fn()
+      dated = createSignal(listenerA)
+      dated.subscribe(listenerB)
+      dated(payload)
+    })
+
+    it('when the signal fires, the listeners receive the payload', () => {
+      expect(listenerA).toHaveBeenCalledWith(payload)
+      expect(listenerB).toHaveBeenCalledWith(payload)
+    })
+  })
+
+  describe('when removing a listener before firing', () => {
+    let listenerA: jest.Mock<any>
+    let removeA: Unsubscriber
+    beforeEach(() => {
+      listenerA = jest.fn()
+      removeA = dated.subscribe(listenerA)
+      removeA()
+      dated(new Date())
+    })
+
+    it('the listener is not called', () => {
+      expect(listenerA).not.toBeCalled()
+    })
   })
 })
